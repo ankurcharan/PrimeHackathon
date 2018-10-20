a3a3de70a33800cee52298ede8a669ad6551d6c3
Ankur Charan <ankurcharan98@gmail.com>
2018-10-20 17:02:53 +0530

    adding projects
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 const googleUrl = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=';
 const users = 'users';
 const techStack = "techStack";
+const projectsStack = "projectStack";
 
 
 // ROUTES
@@ -31,6 +32,8 @@ app.post('/login', googleLogin);
 app.put('/onBoard', isAuthenticated, onBoard);
 // techStack
 app.post('/user/techstack', isAuthenticated, addTechStack);
+app.post('/user/projects', isAuthenticated, addProjects);
+
 
 // database constant
 const usersCollection = db.collection(users);
@@ -44,7 +47,96 @@ app.use('/', function (req, res) {
 
 
 
+// to add user projects
 
+// raw json in body
+//
+// {
+// 	"projects": [
+// 		{
+// 			"projectTitle": "Bridge",
+// 			"projectDescription": "A bridge between the retailer and the online commerce.",
+// 			"projectRepo": "https://github.com/ankurcharan/Bridge",
+// 			"references":[
+// 				{
+// 					"link": "www.udemy.com",
+// 					"remark": "free online courses"
+// 				},
+// 				{
+// 					"link": "www.youtube.com",
+// 					"remark": "best video platform"
+// 				}
+				
+// 			],
+// 			"status": "Completed",
+// 			"techStack": ["Firebase Functions", "NodeJS", "Javascript", "CSS"]
+// 		},
+// 		{
+// 			"projectTitle": "Stack-Shetra",
+// 			"projectDescription": "Internet discussion form for college students",
+// 			"projectRepo": "https://github.com/ankurcharan/HackPrime",
+// 			"references":[
+// 				{
+// 					"link": "www.udemy.com",
+// 					"remark": "free online courses"
+// 				},
+// 				{
+// 					"link": "www.youtube.com",
+// 					"remark": "best video platform"
+// 				}
+				
+// 			],
+// 			"status": "Ongoing",
+// 			"techStack": ["Firestore", "NodeJS", "JS", "HTML"]
+// 		}
+// 	]
+// }
+
+
+function addProjects(req, res) {
+
+	let sub = req.body.sub;
+
+	let projectStack = usersCollection.doc(sub).collection(projectsStack);
+
+	let projects = req.body.projects;
+
+	let promises = [];
+
+	for(let project in projects) {
+
+		console.log("----------");
+		console.log("----------");
+		// console.log(projects[project]);
+
+		let title = projects[project]["projectTitle"].toLowerCase();
+
+		let x = projectStack.doc(title).set(projects[project]);
+		// console.log();
+
+		promises.push(x);
+		console.log("----------");
+		console.log("----------");
+	}
+
+	Promise.all(promises)
+	.then(() => {
+
+		return res.status(200).json({
+			success: true,
+			message: "projects added succesfully"
+		})
+	})
+	.catch((err) => {
+
+		return res.status(500).json({
+			success: false,
+			message: "could not add projects",
+			err: err
+		})
+	})
+	
+}
 
 
 
